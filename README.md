c983b28bf9ab0916a8a4b7f1f7e9d4368597ceb4
Anna Stefaniv <happyasme11@gmail.com>
2024-08-14 10:38:05 -0700

    Added docstrings

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 scraping from https://wallawalla.smartcatalogiq.com/en/current/undergraduate-bulletin/courses/
+
+also add corequisites
--- main.py
+++ main.py
@@ -2,21 +2,33 @@ from playwright.sync_api import sync_playwright
 import json
 
 def get_department_links(page) -> list:
+    """
+    Extract from the page links to all the departments
+    """
     departments = page.query_selector_all('.sc-child-item-links li a')
     department_links = ["https://wallawalla.smartcatalogiq.com" + d.get_attribute('href') for d in departments]
     return department_links
 
 def get_course_links(page) -> list:
+    """
+    Extract from the page links to all the courses
+    """
     courses = page.query_selector_all('.sc-child-item-links li a')
     course_links = ["https://wallawalla.smartcatalogiq.com" + c.get_attribute('href') for c in courses]
     return course_links
 
 def get_prereq_codes(page) -> list:
+    """
+    Extract the codes of all prerequisites on that class page
+    """
     if page.query_selector('h3:has-text("Prerequisite")'):
         prereqs = page.query_selector_all(".sc_prereqs a")
         return [e.inner_html() for e in prereqs]
         
 def course_code(url) -> str:
+    """
+    Extract the course code from the URL
+    """
     value = url.split('/')[-1].split('-')
     course_code, course_number = value[0], value[1]
     return f"{course_code.upper()} {course_number}"
@@ -25,7 +37,6 @@ def prereq_dict() -> dict:
     """
     Returns a dictionary with class code as the key and a list of class codes of the prerequisites as the value.
     """
-
     with sync_playwright() as p:
         # Launch browser
         browser = p.chromium.launch()
@@ -38,7 +49,7 @@ def prereq_dict() -> dict:
         course_prereqs = {}
 
         departments = get_department_links(page)
-        departments = departments[:2] #FOR TESTING
+        # departments = departments[:2] #FOR TESTING
 
         for department in departments:
             page.goto(department)
@@ -54,4 +65,8 @@ def prereq_dict() -> dict:
     
 if __name__ == "__main__":
     prereqs = prereq_dict()
-    print(json.dumps(prereqs, indent=2))
+
+    with open("prereq_dict.json", "w") as f:
+        f.write(json.dumps(prereqs, indent=4))
+    
+    print("Done!")
